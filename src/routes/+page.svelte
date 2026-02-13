be5d7811a96e0513057178eaaedb9ac312b56e26
1402jelorand <jeanfebxl@hotmail.fr>
2026-02-13 14:18:23 +0100

    home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,1 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="text-3XL font-black"><a href="admin/index.html"> See the admin page</a></h1>
